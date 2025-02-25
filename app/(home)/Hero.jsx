674b28621d8dbd4f9dff73fb0370c1c93ddb749d
Asrul Syed <asrulzsyed@gmail.com>
2025-02-26 02:56:34 +0900

    correct animation

--- app/(home)/Hero.jsx
+++ app/(home)/Hero.jsx
@@ -3,108 +3,156 @@
 import { useEffect, useRef } from "react";
 import gsap from 'gsap'
 import { MotionPathPlugin } from "gsap/MotionPathPlugin"
+import Image from "next/image";
 
 gsap.registerPlugin(MotionPathPlugin);
 
+const DOTS_COUNT = 200;
+const STROKE_WIDTH = 10;
+const COLORS = ["#000000", "#FFFFFF"]
+
+const createAnimatedGradient = (container, pathSelector) => {
+  const gradientPath = document.querySelector(pathSelector);
+  // Calculate the number of dots based on the path length.
+  const baseNumberOfDots = Math.ceil(0.5 * gradientPath.getTotalLength());
+  const numberOfDots = 2 * baseNumberOfDots;
+
+  for (let i = 0; i < DOTS_COUNT; i++) {
+    const circle = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
+    container.appendChild(circle);
+    let radius = 0.5 * STROKE_WIDTH + (0.02 * i - 0.6 * STROKE_WIDTH);
+    radius = Math.max(0, radius);
+
+    gsap.set(circle, {
+      attr: {
+        cx: 0,
+        cy: 0,
+        r: radius,
+        fill: gsap.utils.interpolate(COLORS, i / DOTS_COUNT),
+      }
+    });
+
+    gsap.to(circle, {
+      duration: 16,
+      ease: 'none',
+      repeat: -1,
+      immediateRender: true,
+      motionPath: {
+        path: gradientPath
+      }
+    }).progress(i / numberOfDots);
+  }
+};
+
 const Hero = () => {
   useEffect(() => {
-    const strokeWidth = 10;
-    const colors = ["#000000", "#FFFFFF"];
-    const gradientPath = document.querySelector("#gradient-path");
-    const baseNumberOfDots = Math.ceil(.5 * gradientPath.getTotalLength());
+    const topContainer = document.querySelector(".top_animated_gradient_els");
+    const bottomContainer = document.querySelector(".bottom_animated_gradient_els");
 
-    function createAnimatedGradient(g) {
-      const numberOfDots = 2 * baseNumberOfDots;
-
-      for (let idx = 0; idx < 200; idx++) {
-        const circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-        g.appendChild(circle);
-        let radius;
-        radius = .5 * strokeWidth;
-        radius += (.02 * idx - .6 * strokeWidth);
-        radius = Math.max(0, radius);
-        gsap.set(circle, {
-          attr: {
-            cx: 0,
-            cy: 0,
-            r: radius,
-            fill: gsap.utils.interpolate(colors, (idx / 200)),
-          },
-        })
-
-        gsap.to(circle, {
-          motionPath: {
-            path: gradientPath,
-          },
-          duration: 16,
-          ease: "none",
-          repeat: -1,
-        }).progress(idx / numberOfDots);
-      }
+    if (topContainer && bottomContainer) {
+      createAnimatedGradient(topContainer, "#top_orbit");
+      createAnimatedGradient(bottomContainer, "#bottom_orbit");
     }
-
-    createAnimatedGradient(document.querySelector(".animated-gradient-els"));
   }, []);
 
   return (
-    <div className="hero-bg max-w-[1154px] w-full pt-[80px] pb-[50px] flex flex-col justify-center items-center relative px-6 text-center min-h-screen">
-      <div className='relative'>
-        <svg width="959" height="609" viewBox="0 0 959 609" fill="none" xmlns="http://www.w3.org/2000/svg" className="flex items-center py-3 scale-y-125">
+    <div className="hero-bg w-full pt-[80px] pb-[50px] flex flex-col justify-center items-center relative px-6 text-center min-h-screen">
+      <div className='w-full relative flex justify-center items-center'>
+        <svg width="1500" height="780" viewBox="0 0 1500 780" fill="none" xmlns="http://www.w3.org/2000/svg" className="">
+          <path d="M827.639 262.792C827.639 248.919 766.369 188.142 430.344 149.497C36.2967 104.179 188.395 55.1614 287.448 50.0747C386.501 44.9879 1319.11 -17.4401 1340.22 6.60627" stroke="url(#top_left)" />
+          <path d="M655.384 497.324C655.384 511.197 594.048 571.974 257.656 610.619C-136.821 655.937 15.4431 704.955 114.604 710.041C213.765 715.128 1147.4 777.556 1168.53 753.51" stroke="url(#bottom_left)" />
+          <path d="M1342.11 8.71669C1088.95 23.5136 573.745 62.0781 538.209 97.9607C493.789 142.814 1297.69 215.874 1382.19 301.881" stroke="url(#top_right)" />
+          <path d="M1169.2 752.485C916.422 737.688 401.994 699.124 366.511 663.241C322.158 618.388 1124.85 545.328 1209.23 459.321" stroke="url(#bottom_right)" />
+          <path d="M639.241 249.585C639.241 225.191 570.704 139.937 536.435 100.359C560.784 54.6208 1302.61 11.1928 1342.1 9.80677C1373.7 8.69796 1393.87 224.329 1400 332.283C1392.64 271.299 889.761 251.741 639.241 249.585Z" fill="url(#top_gradient)" />
+          <path d="M466.425 511.617C466.425 536.011 397.787 621.265 363.468 660.843C387.853 706.581 1130.77 750.009 1170.32 751.395C1201.97 752.504 1222.16 536.873 1228.3 428.919C1220.94 489.903 717.314 509.461 466.425 511.617Z" fill="url(#bottom_gradient)" />
+          <path d="M317.359 567.707C328.614 306.219 226.454 138.211 173.967 86.8938C267.217 150.694 457.288 354.023 471.573 656.935" stroke="url(#side_gradient)" />
           <defs>
-            <linearGradient id="mergedGradient" x1="100%" y1="0" x2="0" y2="100%" gradientUnits="userSpaceOnUse">
-              <stop stopColor="#D6D6D6" stopOpacity="0.2" />
-              <stop offset="0.5" stopColor="#D6D6D6" />
-              <stop offset="1" stopColor="#D6D6D6" stopOpacity="0.2" />
+            <linearGradient id="top_left" x1="798.41" y1="-64.6079" x2="768.161" y2="263.812" gradientUnits="userSpaceOnUse">
+              <stop stop-color="white" stop-opacity="0" />
+              <stop offset="0.5" stop-color="white" />
+              <stop offset="1" stop-color="white" stop-opacity="0" />
             </linearGradient>
-            <linearGradient id="paint4_linear_1339_8161" x1="666.5" y1="7" x2="666.5" y2="263" gradientUnits="userSpaceOnUse">
-              <stop stopColor="#D6D6D6" stopOpacity="0.08" />
-              <stop offset="1" stopColor="#060606" stopOpacity="0" />
+            <linearGradient id="bottom_left" x1="626.124" y1="824.724" x2="595.907" y2="496.299" gradientUnits="userSpaceOnUse">
+              <stop stop-color="white" stop-opacity="0" />
+              <stop offset="0.5" stop-color="white" />
+              <stop offset="1" stop-color="white" stop-opacity="0" />
             </linearGradient>
-            <linearGradient id="paint5_linear_1339_8161" x1="549" y1="596" x2="549" y2="339" gradientUnits="userSpaceOnUse">
-              <stop stopColor="#D6D6D6" stopOpacity="0.08" />
-              <stop offset="1" stopColor="#060606" stopOpacity="0" />
+            <linearGradient id="top_right" x1="1218.6" y1="-8.39227" x2="1202.52" y2="328.107" gradientUnits="userSpaceOnUse">
+              <stop stop-color="white" stop-opacity="0" />
+              <stop offset="0.5" stop-color="white" />
+              <stop offset="1" stop-color="white" stop-opacity="0" />
             </linearGradient>
-            <linearGradient id="paint6_linear_1339_8161" x1="320.296" y1="436.029" x2="166.179" y2="53.6134" gradientUnits="userSpaceOnUse">
-              <stop stopColor="#D6D6D6" stopOpacity="0" />
-              <stop offset="1" stopColor="#D6D6D6" stopOpacity="0.4" />
+            <linearGradient id="bottom_right" x1="1045.88" y1="769.594" x2="1029.78" y2="433.098" gradientUnits="userSpaceOnUse">
+              <stop stop-color="white" stop-opacity="0" />
+              <stop offset="0.5" stop-color="white" />
+              <stop offset="1" stop-color="white" stop-opacity="0" />
             </linearGradient>
-
-            <path id="gradient-path" d="M830 364 C772.747 432.063 228.107 489.88 258.202 525.375 C282.277 553.772 631.326 584.29 802.841 596 C777.683 617.179 145.148 567.795 77.9668 563.771 C10.7853 559.747 -92.3734 520.972 174.884 485.123 C402.789 454.552 561 256.474 561 245.5 C575 271.473 529.789 148.662 301.884 117.944 C34.6266 81.9221 137.785 42.9592 204.967 38.9159 C272.148 34.8726 904.683 -13.6138 919 5.50001 C747.485 17.2098 400.277 49.7284 376.202 78.1246 C346.107 113.62 890.747 169.937 948 238 L830 364" />
-            <mask id="gradient-path-clip">
-              <use xlinkHref="#gradient-path" strokeWidth="20" fill="none" stroke="white" />
+            <linearGradient id="top_gradient" x1="968.218" y1="9.80251" x2="968.218" y2="332.283" gradientUnits="userSpaceOnUse">
+              <stop stop-color="white" stop-opacity="0.08" />
+              <stop offset="1" stop-color="#060606" stop-opacity="0" />
+            </linearGradient>
+            <linearGradient id="bottom_gradient" x1="795.886" y1="751.399" x2="795.886" y2="428.919" gradientUnits="userSpaceOnUse">
+              <stop stop-color="white" stop-opacity="0.08" />
+              <stop offset="1" stop-color="#060606" stop-opacity="0" />
+            </linearGradient>
+            <linearGradient id="side_gradient" x1="455.881" y1="548.752" x2="283.06" y2="49.1564" gradientUnits="userSpaceOnUse">
+              <stop stop-color="white" stop-opacity="0" />
+              <stop offset="1" stop-color="white" stop-opacity="0.4" />
+            </linearGradient>
+            <path id="top_orbit" d="M827.639 262.792C827.639 248.919 766.369 188.142 430.344 149.497C36.2967 104.179 188.395 55.1614 287.448 50.0747C386.501 44.9879 1319.11 -17.4401 1340.22 6.60627 M1382.19 301.881 C1297.69 215.874 493.789 142.814 538.209 97.9607 C573.745 62.0781 1088.95 23.5136 1342.11 8.71669" />
+            <path id="bottom_orbit" d="M655.384 497.324C655.384 511.197 594.048 571.974 257.656 610.619C-136.821 655.937 15.4431 704.955 114.604 710.041C213.765 715.128 1147.4 777.556 1168.53 753.51 M1209.23 459.321 C1124.85 545.328 322.158 618.388 366.511 663.241 C401.994 699.124 916.422 737.688 1169.2 752.485" />
+            <mask id="#top_gradient_path_clip">
+              <use xlinkHref="#top_orbit" strokeWidth="20" fill="none" stroke="white" />
             </mask>
+            <mask id="bottom_gradient_path_clip">
+              <use xlinkHref="#bottom_orbit" strokeWidth="20" fill="none" stroke="white" />
+            </mask>
+
           </defs>
 
-          <path d="M443.643 197.35C443.643 177.986 397.214 110.307 374 78.8878C390.494 42.5788 893.023 8.10365 919.78 7.00338C941.186 6.12316 954.846 177.301 959 263C954.015 214.588 613.352 199.062 443.643 197.35Z" fill="url(#paint4_linear_1339_8161)" />
-          <path d="M325.762 404.906C325.762 424.346 279.254 492.29 256 523.831C272.523 560.282 775.91 594.892 802.713 595.997C824.156 596.88 837.839 425.034 842 339C837.007 387.601 495.761 403.188 325.762 404.906Z" fill="url(#paint5_linear_1339_8161)" />
-          <path d="M225.809 451.092C233.486 243.293 163.802 109.781 128 69C191.607 119.701 321.256 281.282 331 522" stroke="url(#paint6_linear_1339_8161)" />
-
-          <path id="mainPath" d="M830 364 C772.747 432.063 228.107 489.88 258.202 525.375 C282.277 553.772 631.326 584.29 802.841 596 C777.683 617.179 145.148 567.795 77.9668 563.771 C10.7853 559.747 -92.3734 520.972 174.884 485.123 C402.789 454.552 561 256.474 561 245.5 C575 271.473 529.789 148.662 301.884 117.944 C34.6266 81.9221 137.785 42.9592 204.967 38.9159 C272.148 34.8726 904.683 -13.6138 919 5.50001 C747.485 17.2098 400.277 49.7284 376.202 78.1246 C346.107 113.62 890.747 169.937 948 238 L830 364" stroke="url(#mergedGradient)" />
-
-          <g mask="url(#gradient-path-clip)" className="animated-gradient-els">
+          <g mask="url(#top_gradient_path_clip)" className="top_animated_gradient_els">
+          </g>
+          <g mask="url(#bottom_gradient_path_clip)" className="bottom_animated_gradient_els">
           </g>
         </svg>
 
-        <div className="absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 flex flex-col gap-9 items-center max-w-[1040px] w-full mb-10 py-3 bg-gradient-to-r from-transparent via-[#000000] to-[#000000]/100">
+        <div className="absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 flex flex-col gap-16 items-center max-w-[1040px] w-full mb-10 py-3 px-10">
           <h1 className="text-textButton font-bold sm:!text-[48px] !text-[24px]">AI-Powered Crypto Trading and Investment Platform</h1>
           <div className='flex gap-5'>
             <button
-              className={`bg-textButton text-textWhiteButton hover:scale-105
-border rounded-lg cursor-pointer transition-all duration-200 ease-in-out h-[46px] w-[170px] flex justify-center items-center  text-base  tracking-[0.32px] group relative`}
+              className={`bg-textButton text-textWhiteButton hover:scale-105 border rounded-lg cursor-pointer transition-all duration-200 ease-in-out h-[46px] w-[170px] flex justify-center items-center  text-base  tracking-[0.32px] group relative`}
             >
               Get Started
             </button>
             <button
-              className={` border-textHeader text-textFooterTitle bg-black hover:scale-105
-border rounded-lg cursor-pointer transition-all duration-200 ease-in-out h-[46px] w-[170px] flex justify-center items-center  text-base  tracking-[0.32px] group relative`}
+              className={` border-textHeader text-textFooterTitle bg-black hover:scale-105 border rounded-lg cursor-pointer transition-all duration-200 ease-in-out h-[46px] w-[170px] flex justify-center items-center  text-base  tracking-[0.32px] group relative`}
             >
               Learn More
             </button>
           </div>
         </div>
       </div>
-
-
+      <Image
+        src="/grid.png"
+        alt="grid"
+        width={1920}
+        height={1080}
+        className="w-full h-auto object-cover absolute right-0 bottom-0 left-0"
+      />
+      <Image
+        src="/hero-light.png"
+        alt="hero"
+        width={559}
+        height={465}
+        className="w-[500px] h-auto -z-[1] object-cover absolute top-0 left-0 filter blur-xl hero-light"
+      />
+      <Image
+        src="/hero-light-right.png"
+        alt="hero"
+        width={559}
+        height={465}
+        className="w-[500px] h-auto -z-[1] object-cover absolute top-0 right-0 filter blur-xl hero-light1"
+      />
     </div >
   )
 }
